354b504af013c98a95440a09d3614dc7c04d7b8d
Ed Page <epage@duosecurity.com>
2019-11-12 16:13:10 -0700

    Dont rely on submodule for hardware
    Still using it for development because searching it is convinient.

--- machines/lenovo-t580/default.nix
+++ machines/lenovo-t580/default.nix
@@ -1,8 +1,8 @@
 {
   imports = [
-    ../nixos-hardware/lenovo/thinkpad
-    ../nixos-hardware/common/cpu/intel
-    ../nixos-hardware/common/pc/laptop/ssd
+    <nixos-hardware/lenovo/thinkpad>
+    <nixos-hardware/common/cpu/intel>
+    <nixos-hardware/common/pc/laptop/ssd>
   ];
 
   services.fprintd.enable = true;
